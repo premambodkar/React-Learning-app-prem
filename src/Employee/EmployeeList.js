d5d6ae176b9a20e17c55cc25f89d250cee9aa64f
premambodkar <prem.ambodkar@gmail.com>
2022-12-20 13:05:58 +0530

    1 file modified

--- src/Employee/EmployeeList.js
+++ src/Employee/EmployeeList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import CustomTable from '../common-components/CustomTable.js';
-import Link from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
 function EmployeeList() {
+  const history = useHistory();
   const dataRows = [
     { id: 1, name: 'a', salary: 10 },
     { id: 2, name: 'b', salary: 15 },
@@ -11,14 +13,22 @@ function EmployeeList() {
     { id: 6, name: 'f', salary: 7 },
   ];
   const columns = ['id', 'name', 'salary'];
+
   const onEdit = () => {
     alert('Test');
   };
+
+  const handleClick = () => {
+    history.push('/employee');
+  };
+
   return (
     <div className="container">
       {/* <Link to="employee">About Us</Link> */}
       <div className="d-flex flex-row-reverse">
-        <button className="btn btn-primary">New Employee</button>
+        <button className="btn btn-primary" onClick={handleClick}>
+          New Employee
+        </button>
       </div>
       <CustomTable columns={columns} dataRows={dataRows} onEdit="onClick" />
     </div>
